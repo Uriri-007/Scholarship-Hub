ff220b91dfb32ef6ec3d31c68d704cb36a767c30
Uri Okhai <uriokhai@gmail.com>
2025-12-02 18:59:28 +0100

    Add some placeholder content for the main

--- src/components/Scholarships.jsx
+++ src/components/Scholarships.jsx
@@ -14,23 +14,41 @@ export default function Scholarships(props) {
   const TABLE_ID = import.meta.env.VITE_AIRTABLE_TABLE_ID
   const API_URL = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_ID}`
   
-
-useEffect(() => {
+  useEffect(() => {
     axios.get(API_URL, {
         headers: {
-            "Authorization": `Bearer ${AIRTABLE_PAT}`,
+            Authorization: `Bearer ${AIRTABLE_PAT}`,
         },
     })
     .then(response => {
         setData(response)
-        alert(`data ${data}`)
         return
     })
     .catch(err => {
         setError(err)
-        alert(`error ${error}`)
         return
     });
 }, []);
+
+  return (
+    <main>
+      <div className="scholarship">
+        <div>
+          <h2>Jim Ovia Foundation Scholarship 2025</h2>
+          <span>Ongoing</span>
+        </div>
+        
+        <p>Provides comprehensive financial support for brilliant, indigent Nigerian students throughout their undergraduate studies.</p>
+        
+        <div className="features">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        
+        <a href="https://jimoviafoundation.org/2025-jim-ovia-scholarship-application-new-applicants/" rel="noopener noreferrer" target="_blank">Go to page</a>
+      </div>
+    </main>
+    )
 
 }
